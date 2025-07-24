439b1ce6332ecf72dc28f735098677f09e36c5d2
ghkdtjfxkd <syt1128@gmail.com>
2025-07-24 15:07:59 +0900

    Initial project setup

---
+++ src/main/java/com/grewmeet/dating/datingcommandservice/DatingCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grewmeet.dating.datingcommandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingCommandServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingCommandServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grewmeet/dating/datingcommandservice/DatingCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grewmeet.dating.datingcommandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingCommandServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
